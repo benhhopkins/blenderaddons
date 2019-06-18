22a00b8d42a8fcf1e739a77751d8d6694cd8db30
Ben Hopkins <ben@bhopkins.net>
2019-06-18 09:55:30 -0500

    v1.0 Unwrap Face Pixel Scaled

--- Unwrap Face Pixel Scaled.py
+++ Unwrap Face Pixel Scaled.py
@@ -10,11 +10,22 @@ bl_info = {
     "category": "Viewpoint",
 }
 
+# TODO: Unwrap multiple faces together
 
 import bpy
 import bmesh
 import math
 import mathutils
+
+def loopSortValue(loops, nLoop):
+        p1 = loops[nLoop].vert.co
+        loop2 = nLoop + 1
+        if loop2 == len(loops):
+            loop2 = 0
+        p2 = loops[loop2].vert.co
+    
+        value = p1.x + p2.x + 100 * (p1.y + p2.y) + 1000 * (p1.z + p2.z)
+        return value
 
 class UV_MT_unwrap_pixel(bpy.types.Operator):
     """Point view towards the current face"""
@@ -29,11 +40,11 @@ class UV_MT_unwrap_pixel(bpy.types.Operator):
         description="Pixels per meter",
     )
     
-    textureSizeMeters: bpy.props.FloatProperty(
-        name="Texture Size (meters)",
-        default=4,
+    correctForScale: bpy.props.BoolProperty(
+        name="Correct for unapplied Object Scale",
+        default=True,
         subtype='NONE',
-        description="Size of one side of the texture in meters",
+        description="Corrects the UVs as if the object has a scale of 1.",
     )
 
     @classmethod
@@ -45,12 +56,13 @@ class UV_MT_unwrap_pixel(bpy.types.Operator):
         mesh = context.object.data
         if not mesh.is_editmode:
             self.report({'INFO'}, 'Must be in edit mode')
-            return {'CANCELLED'}
+            return {'CANCELLED'}        
 
-        obj = context.edit_object
+        objectScale = context.edit_object.scale
+        if objectScale.x != 1.0 and objectScale.y != 1.0 and objectScale.z != 1.0:
+            self.report({'WARNING'}, 'Warning: Object scale is not 1.0!')
+
         bm = bmesh.from_edit_mesh(mesh)
-
-#        rv3d = context.space_data.region_3d
         
         bm = bmesh.from_edit_mesh(mesh)
         bm.faces.ensure_lookup_table()
@@ -59,38 +71,56 @@ class UV_MT_unwrap_pixel(bpy.types.Operator):
         if bm.faces.active is not None:
             active = bm.faces.active
             center = active.calc_center_median()
-            nLoops = len(bm.faces.active.loops)
-            n = active.normal
-            print('normal:')
-            print(n)
-            i = n.to_3d()
-            eul = mathutils.Euler((0, math.radians(90.0), math.radians(90.0)), 'XYZ')
-            i.rotate(eul)
-            i = n.cross(i)
-            print('i:')
-            print(i)
-            j = n.to_3d()
-            eul = mathutils.Euler((math.radians(90.0), 0, math.radians(90.0)), 'XYZ')
-            j.rotate(eul)
-            j = n.cross(j)
-            print('j:')
-            print(j)
-            for li in range(nLoops):
-                loop = active.loops[li]
-                vert = loop.vert
-                d = vert.co - center
-                u = d.dot(i) * (self.textureSizeMeters / self.pixelScale)
-                v = d.dot(j) * (self.textureSizeMeters / self.pixelScale)
-                uv = (u, v)
-                print(uv)
-                active.loops[li][uv_layer].uv = uv
+            loops = active.loops
             
-            '''
-            active.loops[0][uv_layer].uv = (0, 0)
-            active.loops[1][uv_layer].uv = (0, 1)
-            active.loops[2][uv_layer].uv = (1, 1)
-            active.loops[3][uv_layer].uv = (1, 0)
-            '''
+            # find the bottom two verticies
+            nLoops = len(loops)
+            firstBottomLoop = 0
+            bottomValue = loopSortValue(loops, 0)
+            for li in range(nLoops):
+                testValue = loopSortValue(loops, li)
+                if testValue < bottomValue:
+                    bottomValue = testValue
+                    firstBottomLoop = li;
+            
+            # the "x-axis" is the bottom verticies vector
+            secondBottomLoop = firstBottomLoop + 1
+            if secondBottomLoop == len(loops):
+                secondBottomLoop = 0
+            x = loops[secondBottomLoop].vert.co - loops[firstBottomLoop].vert.co
+            x.normalize()
+            
+            # get the "y-axis" from the bottom verticies vector
+            y = x.cross(active.normal)
+            y.normalize()
+            y.negate()
+            
+            # find texture scale and center of current UV screen area
+            offset = [0.5, 0.5]
+            scale = self.pixelScale / 128.0
+            roundSize = 128.0
+            for area in bpy.context.screen.areas :
+                if area.type == 'IMAGE_EDITOR':
+                    currentTexture = area.spaces.active.image
+                    scale = self.pixelScale / currentTexture.size[1]
+                    roundSize = currentTexture.size[1]
+                    for region in area.regions:
+                        if region.type == 'WINDOW':
+                            offset = region.view2d.region_to_view(region.width / 2, region.height / 2)
+            
+            # calculate UVs
+            for li in range(nLoops):
+                loop = loops[li]
+                d = loop.vert.co - center
+                if self.correctForScale:
+                    d.x *= objectScale.x
+                    d.y *= objectScale.y
+                    d.z *= objectScale.z
+                u = d.dot(x) * scale + offset[0]
+                v = d.dot(y) * scale + offset[1]
+                uv =   (round(u * roundSize) / roundSize,
+                        round(v * roundSize) / roundSize)
+                loop[uv_layer].uv = uv
         else:
             self.report({'INFO'}, 'Must have face selected')
             return {'CANCELLED'}
